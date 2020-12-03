a60d0601a57c9aea3ebb39ea03324b5019e29629
DFilippov <porkcork@yandex.ru>
2020-12-03 22:49:51 +0300

    Implement search in articles titles & text also in comments body

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
+  # get 'search' => 'search#index'
+  # get 'search/index', to: 'search#index'
+  get 'search/index', action: :index, controller: 'search'
+  # resource :search, only: [:index]
 
   # здесь модель Article но прописываем articles (во множественном числе)
   resources :articles  do
