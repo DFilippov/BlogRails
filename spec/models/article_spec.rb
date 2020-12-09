558e56f2952d6b0e86ef52a4f96775cba3c0763d
DFilippov <porkcork@yandex.ru>
2020-12-09 16:21:57 +0300

    Update article_spec.rb - add name of describe method

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,6 +4,9 @@ describe Article do
         describe 'validations' do
                 it { should validate_presence_of :title }
                 it { should validate_presence_of :text }
+        end
+
+        describe 'associations' do
                 it { should have_many :comments }
         end
  end
