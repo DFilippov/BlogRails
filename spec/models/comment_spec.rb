79eb211531e1174eeb428a6fdb4818b4b4194a47
DFilippov <porkcork@yandex.ru>
2020-12-10 10:33:54 +0300

    Update comment_spec.rb - add describe associations

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,10 +3,11 @@ require 'rails_helper'
 describe Comment do
 
         describe 'validations' do
-
-                it { should belong_to :article }
                 it { should validate_presence_of :body }
+        end
 
+        describe 'associations' do
+                it { should belong_to :article }
         end
 
 end
