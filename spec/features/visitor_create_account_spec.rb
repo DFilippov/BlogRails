1c9cd7a7d82671d924d6bcc15702d85d3691362d
DFilippov <porkcork@yandex.ru>
2020-12-14 23:36:16 +0300

    Move method sign_up to ./spec/support/session_helper.rb

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -1,20 +1,11 @@
 require 'rails_helper'
+require './spec/support/session_helper'
 
 feature 'Account Creation' do
         scenario 'allows guest to create account' do
-                # visit '/users/sign_up'
-                visit new_user_registration_path
-
-                password = 'nobody'
-
-                fill_in :user_email, with: 'user@example.com'
-                fill_in :user_username, with: 'username'
-                fill_in :user_password, with: password
-                fill_in :user_password_confirmation, with: password
-                click_button 'Sign up'
-
-                # expect(page).to have_content 'Welcome! You have signed up successfully'
+                sign_up
                 expect(page).to have_content( I18n.t 'devise.registrations.signed_up' )
         end
 
+        
 end
