c690774ff8efda3bdd7bc4151a2ef4b32d5dd0fb
DFilippov <porkcork@yandex.ru>
2020-12-15 22:42:07 +0300

    Test visitor creates account

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 require './spec/support/session_helper'
+require 'database_cleaner/active_record'
+
+DatabaseCleaner.start
 
 feature 'Account Creation' do
+
+        after(:all) do
+                DatabaseCleaner.clean
+        end
+
         scenario 'allows guest to create account' do
+
                 sign_up
                 expect(page).to have_content( I18n.t 'devise.registrations.signed_up' )
+
         end
+
 
         
 end
